6e0bf0d900b325528f06d539c2e15238af102b37
yangsophieee <sophie.yang917@gmail.com>
2023-12-12 00:06:03 +1100

    Update code and add files

--- R/make_aus_native_species_list.r
+++ R/make_aus_native_species_list.r
@@ -1,13 +1,13 @@
+library(APCalign)
+resources <- load_taxonomic_resources()
+apc <- resources$APC
 
-#apc <- read_csv("raw_data/APC-taxon-2021-06-08-0734.csv")
-apc <- read_csv("data/taxon_list.csv", guess_max = 42451)
-
-apcs <- 
-  apc %>% 
+apcs <-
+  apc %>%
   filter(
-    taxon_rank %in% c("Species", "Forma", "Varietas", "Subspecies"),
+    taxon_rank %in% c("species", "form", "variety", "subspecies"),
     taxonomic_status == "accepted"
-    )
+  )
 
 apcs$nat_count <- str_count(apcs$taxon_distribution, "naturalised")
 apcs$comma_count <- str_count(apcs$taxon_distribution, ",")
@@ -20,13 +20,13 @@ apcs$taxon_distribution[x][1]
 apcs$nat_count[x][1]
 apcs$comma_count[x][1]
 
-sum(apcs$native_somewhere_index, na.rm = T)
+sum(apcs$native_somewhere_index, na.rm = TRUE)
 dim(apcs)
 
 apcs %>%
-  mutate(aus_native = apcs$native_somewhere_index,
-         aus_native = if_else(str_detect(taxon_distribution, "native"), TRUE, aus_native)) %>%
-  select(taxon_name, aus_native) %>%
-  distinct() %>% 
+  mutate(
+    aus_native = apcs$native_somewhere_index,
+    aus_native = if_else(str_detect(taxon_distribution, "native"), TRUE, aus_native)) %>%
+  select(canonical_name, aus_native) %>%
+  distinct() %>%
   write_csv("data/aus_native_lookup.csv")
-
