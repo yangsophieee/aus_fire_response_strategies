147d29541d0cc4d65bb582c956a26cc9658de6f1
yangsophieee <sophie.yang917@gmail.com>
2024-03-02 12:47:38 +1100

    Make script for aligning GBIF data

---
+++ R/align_phylogeny_with_apcalign.r
@@ -0,0 +1,32 @@
+
+# Load libraries
+library(dplyr)
+library(readr)
+library(forcats)
+library(stringr)
+library(ggplot2)
+library(tibble)
+library(lubridate)
+library(tidyr)
+library(purrr)
+library(APCalign)
+library(ape)
+
+# Align names in tree with APCalign
+tt <- read.tree("data/v0.1-big-seed-plant-trees/ALLMB.tre")
+tt$tip.label <- gsub("_", " ", tt$tip.label)
+
+tt <- keep.tip(tt, tt$tip.label)
+
+resources <- load_taxonomic_resources()
+
+lookup_table <- create_taxonomic_update_lookup(
+  taxa = tt$tip.label,
+  resources = resources
+)
+
+out <- drop.tip(tt, tt$tip.label[is.na(lookup_table$accepted_name)])
+
+# Save
+write_csv(lookup_table, "lookup_table_GBIF.csv")
+write.tree(out, "ALLMB_APC_updated.tre")
