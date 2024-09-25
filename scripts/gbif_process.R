45ee6c4715f1a79825f452d44e6fb0c66d416add
yangsophieee <sophie.yang917@gmail.com>
2024-09-25 21:05:35 +1000

    Regenerate data files and check fraction of woody seeders map

--- scripts/gbif_process.R
+++ scripts/gbif_process.R
@@ -1,48 +1,37 @@
 
 library(data.table)
-library(curl)
-library(zip)
 library(tidyverse)
 library(CoordinateCleaner)
-
+library(arrow)
 
 aus_gbif <- fread("data/gbif/0004407-230530130749713.csv", quote = "")
 
 # Modified from: https://data-blog.gbif.org/post/gbif-filtering-guide/
-intermediate_check <- data.frame(aus_gbif) %>%
-  setNames(tolower(names(.))) %>% # Set lowercase column names to work with CoordinateCleaner
-  filter(!is.na(decimallongitude)) %>%
-  filter(!is.na(decimallatitude)) %>%
-  filter(basisofrecord %in% c("HUMAN_OBSERVATION", "PRESERVED_SPECIMEN")) %>%
-  filter(year >= 1900) %>%
-  filter(coordinateprecision < 0.05 | is.na(coordinateprecision)) %>%
-  filter(coordinateuncertaintyinmeters < 10000 | is.na(coordinateuncertaintyinmeters)) %>%
-  filter(!coordinateuncertaintyinmeters %in% c(301, 3036, 999, 9999)) %>% # Known inaccurate default values
-  filter(!decimallatitude == 0 | !decimallongitude == 0)
-
-# Adding some additional filters
-i2 <- intermediate_check %>%
+intermediate_check <- aus_gbif |>
+  # Already filtered the below on GBIF website
+  filter(countryCode  == "AU") |>
+  filter(occurrenceStatus  == "PRESENT")  |>
+  filter(basisOfRecord %in% c("HUMAN_OBSERVATION", "LIVING_SPECIMEN", "PRESERVED_SPECIMEN")) |>
+  filter(!is.na(decimalLongitude)) |>
+  filter(!is.na(decimalLatitude)) |>
+  filter(establishmentMeans %in% c("INTRODUCED", "INVASIVE", "NATURALISED", "")) |> # Exclude "MANAGED" or "Uncertain"
+  filter(coordinatePrecision < 0.05 | is.na(coordinatePrecision)) |>
+  filter(year >= 1900) |>
+  filter(coordinateUncertaintyInMeters < 10000 | is.na(coordinateUncertaintyInMeters)) |>
+  filter(!decimalLatitude == 0 | !decimalLongitude == 0) |>
   filter(
-    !grepl("COUNTRY_COORDINATE_MISMATCH", intermediate_check$issue),
-    !grepl("RECORDED_DATE_UNLIKELY", intermediate_check$issue)
-    )
+    !grepl("COUNTRY_COORDINATE_MISMATCH", issue) &
+      !grepl("RECORDED_DATE_UNLIKELY", issue)
+  )
 
-aus_filt <- i2 %>%
-  cc_sea(ref = buffland) %>%
-  cc_val() %>%
-  cc_equ() %>%
-  cc_gbif() %>%
-  cc_cen(buffer = 2000) %>% # Remove country centroid within 2 km
-  cc_cap(buffer = 2000) %>% # Remove capital centroid within 2 km
-  cc_inst(buffer = 2000) %>% # Remove zoo and herbaria within 2 km
-  distinct(decimallongitude, decimallatitude, specieskey, datasetkey, .keep_all = TRUE)
+aus_filt <- intermediate_check |>
+  # cc_sea(ref = buffland) |> # Not working
+  cc_val() |>
+  cc_equ() |>
+  cc_gbif() |>
+  cc_cen(buffer = 2000) |> # Remove country centroid within 2 km
+  cc_cap(buffer = 2000) |> # Remove capital centroid within 2 km
+  cc_inst(buffer = 2000) |> # Remove zoo and herbaria within 2 km
+  distinct(decimalLongitude, decimalLatitude, species, .keep_all = TRUE)
 
-aus_filt %>%
-  select(
-    species,
-    decimalLongitude = decimallongitude,
-    decimalLatitude = decimallatitude,
-    scientificname,
-    verbatimscientificname
-  ) %>%
-  write_csv("data/filtered_aus_obs.csv")
+aus_filt |> write_parquet("data/filtered_aus_obs.parquet")
